cb05e220efe7e9c51587a2863e7b183104e6f56b
ethan <ksr20612@daum.net>
2022-07-22 07:54:57 +0900

    1. 수정일 : 2022.07.22 2. 수정 내용  - 검사지 제작

---
+++ src/components/question.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import SamplePic from "../assets/sample.jpg";
+import pallette from "../styles/pallette.css";
+import DiscreteSlider from "../components/discreteSlider";
+import { darken } from "polished";
+
+const Question = ({
+
+}) => {
+
+    return (
+        <Box>
+            <Question></Question>
+        </Box>
+    )
+}
+const Box = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    align-items : center;
+    background-color : transparent;
+`
+export default Question;
--- src/containers/part1/scr4.js
+++ src/containers/part1/scr4.js
@@ -6,14 +6,49 @@ import Title from "../../components/title";
 import PageInfo, { getPartTitle, getPageTitle } from "../../app/pageInfo";
 import { motion } from "framer-motion";
 import fadein from "../../styles/framer-motion/fadein";
+import { Paper } from "../../styles/components/paper";
+import Question from "../../components/question";
+
+const questionList = [
+
+]
 
 const Scr4 = () => {
+
+    const handleClick = (index, score) => {
+
+    }
 
     return (
         <>
             <Title title={getPartTitle(1)} subTitle={getPageTitle(1,4)}/>
+            <Box>
+                <Info>
+                    <div>나의 xx는</div>
+                    <div>검사 시간은 10분 내외입니다.</div>
+                </Info>
+                {
+                    questionList.map((question, i)=>{
+                        return <Question content={question} handleClick={(score)=>handleClick(i, score)}/>
+                    })
+                }
+            </Box>
         </>
     )
 }
+const Box = styled(Paper)`
+    padding : 5vh;
+    margin-top : 5vh;
+    font-size : 3.2rem;
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+    justify-content : center;
+    height : 75%;
+    word-break : keep-all;
+`
+const Info = styled.div`
+
+`
 
 export default Scr4;
